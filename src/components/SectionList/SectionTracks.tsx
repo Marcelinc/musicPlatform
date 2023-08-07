718918aa5278db2dcb9679021c552cf10acd08d6
Marcelinc <marcin.c40@wp.pl>
2023-08-07 10:53:41 +0200

    Added two sections at homepage, css style fixes

--- src/components/SectionList/SectionTracks.tsx
+++ src/components/SectionList/SectionTracks.tsx
@@ -10,14 +10,14 @@ const SectionTracks = ({title,list}:SectionProps) => {
   return (
     <section className='section-container'>
         <h2>{title}</h2>
-        <div className='section-list'>
+        <div className='section-list snaps-inline'>
             {list?.map((item => (
                 <div className='section-item' key={item.id}>
                     <div className='section-image'>
                         <img src={`/images/${item.imgUrl}`} alt='Track Image'/>
                     </div>
                     <span className='section-item-name'>{item.name}</span>
-                    <span className='section-item-owner'>{item.artist.map(artist => artist.artistName)}</span>
+                    <span className='section-item-owner'>{item.artist[0].artistName}</span>   
                 </div>)
             ))}
         </div>
