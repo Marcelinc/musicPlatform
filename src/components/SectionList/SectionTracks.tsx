f6652f66cbf4693e436ca02e4e6457cb6bfefcb7
Marcelinc <marcin.c40@wp.pl>
2023-08-15 14:37:58 +0200

    Artist page layout

--- src/components/SectionList/SectionTracks.tsx
+++ src/components/SectionList/SectionTracks.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom'
 import '../../resources/SectionContainer.css'
 
 type SectionProps = {
@@ -7,17 +8,26 @@ type SectionProps = {
 }
 
 const SectionTracks = ({title,list}:SectionProps) => {
+
+    const navigate = useNavigate()
+    const redirectToTrack = (id:number) => {
+        navigate('/track/' + id)
+    }
+    const redirectToArtist = (id:number) => {
+        navigate('/artist/' + id)
+    }
+
   return (
     <section className='section-container'>
         <h2>{title}</h2>
         <div className='section-list snaps-inline'>
             {list?.map((item => (
                 <div className='section-item' key={item.id}>
-                    <div className='section-image'>
+                    <div className='section-image' /*onClick={() => redirectToTrack(item.id)}*/>
                         <img src={`/images/${item.imgUrl}`} alt='Track Image'/>
                     </div>
-                    <span className='section-item-name'>{item.name}</span>
-                    <span className='section-item-owner'>{item.artist[0].artistName}</span>   
+                    <span className='section-item-name' /*onClick={() => redirectToTrack(item.id)}*/>{item.name}</span>
+                    <span className='section-item-owner' onClick={() => redirectToArtist(item.artist[0].id)}>{item.artist[0].artistName}</span>   
                 </div>)
             ))}
         </div>
